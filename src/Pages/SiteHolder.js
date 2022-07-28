0dbe4ae838dbc6c68f14cb067c2b498f07ad7a12
Jezebel <dg.jezebel@gmail.com>
2022-07-28 19:26:48 -0400

    added facebook and instagram links

--- src/Pages/SiteHolder.js
+++ src/Pages/SiteHolder.js
@@ -2,29 +2,41 @@ import React from 'react';
 import ablogo from '../Images/ablogo.svg'
 import '../css/siteholder.css'
 import { Link } from 'react-router-dom';
+import facebook from '../Images/facebook.png'
+import insta from '../Images/insta.png'
+import messenger from '../Images/messenger.png'
 
 export default function SiteHolder(){
     return(
         <div className="contentDiv">
-        <div className='content'>
-            <img className='siteholderimg' src={ablogo} alt='ab catering logo'></img>
-            
-            <div className='center'>
-                <h1>
-                    We Deliver!
-                </h1>
+            <div className='content'>
+                <img className='siteholderimg' src={ablogo} alt='ab catering logo'></img>
                 
-                <h2>
-                    Now serving Philadelphia and the Tri-State area!
+                <div className='center'>
+                    <h1>
+                        We Deliver!
+                    </h1>
+                    
+                    <h2>
+                        Now serving Philadelphia and the Tri-State area!
+                        </h2>
+                    <h2>
+                        Phone number: (215) 792-2197
+                        <br/>
+                        Email: customerservice@ab-catering.com
                     </h2>
-                <h2>
-                    Phone number: (215) 792-2197
-                    <br/>
-                    Email: customerservice@ab-catering.com
-                </h2>
+                    
+                </div>
             </div>
-        </div>
-            <div className='buttoncenter'><Link to='/menu'className='menuButton'>Check out our Menu!!!</Link></div>
+            <div className='buttoncenter'>
+                <Link to='/menu'className='menuButton'>Check out our Menu!!!</Link>
+            </div>
+            <h2>find us on Facebook and Instagram</h2>
+            <div className='medialinks'>
+                <a href='https://facebook.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img src={facebook} alt='facebook link'/></a>
+                <a href='https://instagram.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img src={insta} alt='instagram link'/></a>
+            </div>
+
         </div>
     )
 }
