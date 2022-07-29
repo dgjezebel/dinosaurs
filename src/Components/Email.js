a7d55950c6148b403b0b2f328aa19fb016d87de7
Jezebel <dg.jezebel@gmail.com>
2022-07-28 22:56:18 -0400

    added email clearing

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -1,17 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import{ init } from '@emailjs/browser';
 import '../css/email.css'
+
 init("user_W7CsbdW7BGSlDGi4rbGca");
 
 export const ContactUs = () => {
-  const form = useRef();
 
+  const form = useRef();
+  const [name, setName] = useState('')
+  const [user, setUser] = useState('')
+  const [message, setMessage] = useState('')
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_1psuwpf', 'template_z658s7j', form.current, 'user_W7CsbdW7BGSlDGi4rbGca')
       .then((result) => {
+        setName('')
+        setUser('')
+        setMessage('')
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
@@ -21,12 +28,18 @@ export const ContactUs = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <div className='error'></div>
-      <input className='inputs' type="text" name="user_name" placeholder='Name'/>
+      <input className='inputs' type="text" name="user_name" placeholder='Name'
+        onChange={event => setName(event.target.value)}
+        value={name}/>
       <div className='error'></div>
-      <input className='inputs' type="email" name="user_email" placeholder='E-mail'/>
+      <input className='inputs' type="email" name="user_email" placeholder='E-mail'
+      onChange={event => setUser(event.target.value)}
+      value={user}/>
       <div className='error'></div>
-      <textarea name="message" placeholder='What can we do for you?'/>
-      <div className='inputbox'><input  className='button'type="submit" value="Send"/></div>
+      <textarea name="message" placeholder='What can we do for you?'
+      onChange={event => setMessage(event.target.value)}
+      value={message}/>
+      <div className='inputbox'><input  className='button'type="submit" value="Send" /></div>
     </form>
   );
 };
