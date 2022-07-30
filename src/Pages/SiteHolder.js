c1c0b381f49cedbe3a01f96ed3964ab3e5702146
Jezebel <dg.jezebel@gmail.com>
2022-07-29 22:03:01 -0400

    added differend main page buttons

--- src/Pages/SiteHolder.js
+++ src/Pages/SiteHolder.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ablogo from '../Images/ablogo.svg'
 import '../css/siteholder.css'
 import { Link } from 'react-router-dom';
-import facebook from '../Images/facebook.png'
-import insta from '../Images/insta.png'
-import messenger from '../Images/messenger.png'
+import facebook from '../Images/fbook.png'
+import insta from '../Images/instagram.png'
+import phone from '../Images/phone.png'
 
 export default function SiteHolder(){
     return(
@@ -18,12 +18,14 @@ export default function SiteHolder(){
                     </h1>
                     
                     <h2>
-                        Now serving Philadelphia and the Tri-State area!
+                        Serving the Philadelphia and Tri-State area!
                         </h2>
                     <h2>
-                        <a href="tel:215-792-2197" className='telnumber'>Phone number: <u>(215)792-2197</u></a>
-                        <br/>
                         Email: customerservice@ab-catering.com
+                    </h2>
+                    <h2>
+                        <a href="tel:215-792-2197" className='telnumber'>Phone number: (215) 792-2197</a>
+                        <br/>
                     </h2>
                     
                 </div>
@@ -31,10 +33,10 @@ export default function SiteHolder(){
             <div className='buttoncenter'>
                 <Link to='/menu'className='menuButton'>Check out our Menu!!!</Link>
             </div>
-            <h2>Find us on Facebook and Instagram</h2>
             <div className='medialinks'>
                 <a href='https://facebook.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img src={facebook} alt='facebook link'/></a>
                 <a href='https://instagram.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img src={insta} alt='instagram link'/></a>
+                <a href="tel:215-792-2197" target="_blank" rel="noopener noreferrer"><img src={phone} alt='instagram link'/></a>
             </div>
 
         </div>
