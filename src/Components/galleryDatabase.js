9f4ed19d702032ca02275be8efc5789524440dbf
Jezebel <dg.jezebel@gmail.com>
2022-11-25 14:38:34 -0500

    added images to gallery

---
+++ src/Components/galleryDatabase.js
@@ -0,0 +1,9 @@
+import image1 from '../GalleryImages/image1.jpg'
+import image2 from '../GalleryImages/image2.jpg'
+import image3 from '../GalleryImages/image3.jpg'
+import image4 from '../GalleryImages/image4.jpg'
+let images = [
+image1, image2, image3, image4,
+]
+
+export {images}
--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../css/gallery.css'
-
+import {images} from '../Components/galleryDatabase'
 export default class Gallery extends React.Component {
     render(){
         return(
-            <div className='aboutDiv'>
-                Gallery coming soon
+            <div className='galleryDiv'>
+                <img className='galleryimage' src={images[0]}/>
+                <img className='galleryimage' src={images[1]}/>
+                <img className='galleryimage' src={images[2]}/>
+                <img className='galleryimage' src={images[3]}/>
             </div>
         )
 
