5f1fc1915e6e0c438984f06e7d8bcb226422896c
Jezebel <dg.jezebel@gmail.com>
2022-11-25 17:17:05 -0500

    changed about page info

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -11,8 +11,7 @@ export default class Menu extends React.Component {
                         Our mission is to bring you the best homestyle cooking right to your door. We use only the freshest ingredients, and we add a little bit of love to everything we bake.
                     </h2>
                     <h2> 
-                        We are located in Philadelphia, PA. We serve the tri-state 
-                        area and offer free delivery within a 25-mile radius of our location.
+                        We are located in Philadelphia, PA. We serve Philadelphia and New Jersey, and offer free delivery within a 25-mile radius of our location.
                         We are always up for a challenge and welcome special requests!
                     </h2>
                     <h2>
