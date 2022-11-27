ff6e9c1a51b3bf0e5cbdaa56bd74a6909bb8a3d9
Jezebel <dg.jezebel@gmail.com>
2022-11-26 23:45:13 -0500

    changed small image size

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -5,16 +5,16 @@ export default class Gallery extends React.Component {
     render(){
         return(
             <div className='galleryDiv'>
-                <img className='galleryimage' src={images[0]}/>
-                <img className='galleryimage' src={images[1]}/>
-                <img className='galleryimage' src={images[2]}/>
                 <img className='galleryimage' src={images[3]}/>
                 <img className='galleryimage' src={images[4]}/>
                 <img className='galleryimage' src={images[5]}/>
+                <img className='galleryimage' src={images[0]}/>
                 <img className='galleryimage' src={images[6]}/>
-                <img className='galleryimage' src={images[7]}/>
+                <img className='galleryimage' src={images[2]}/>
                 <img className='galleryimage' src={images[8]}/>
                 <img className='galleryimage' src={images[9]}/>
+                <img className='galleryimage' src={images[1]}/>
+                <img className='galleryimage' src={images[7]}/>
 
              
             </div>
